58848930721fa1f24da32ac4a9492207abe5c2d4
IgnatiusWisesa <45249515+IgnatiusWisesa@users.noreply.github.com>
2020-10-16 14:56:51 +0700

    first commit

---
+++ routes/apiRoutes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./../models');
+
+// get all todos
+router.get("/all", (req,res) => {
+    db.Todo.findAll().then(todos => res.send(todos))
+})
+
+// get single todo
+router.get("/find/:id", (req,res) => {
+    db.Todo.findAll({
+        where: {
+            id: req.params.id
+        }
+    }).then((todo) => res.send(todo))
+})
+
+// post new todo
+router.post("/new", (req,res) => {
+    db.Todo.create({
+        text: req.body.text
+    }).then((submitedTodo) => res.send(submitedTodo))
+})
+
+// delete todo
+router.delete("/delete/:id", (req,res) => {
+    db.Todo.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(() => res.send('deleted'))
+})
+
+// edit todo
+router.put("/edit", (req,res) => {
+    db.Todo.update(
+        {
+            text: req.body.text
+        }, 
+        {
+           where: { id: req.body.id } 
+        }).then(() => res.send('updated'))
+})
+
+module.exports = router
